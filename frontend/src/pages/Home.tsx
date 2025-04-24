00f239ff116428adfa9beece95d6558ffa19f104
Ngan Nguyen <nhnguyen@umass.edu>
2025-04-24 15:28:05 -0400

    modify home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import './Home.css';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
-  return <h1>Home Page</h1>;
+  const navigate = useNavigate();
+  
+  const navigateToPhotobooth = () => {
+    navigate('/photobooth');
+  }
+
+  const navigateToPhotostrip = () => {
+    navigate('/photostrip');
+  }
+
+  return (
+    <div className="home-page">
+      <div className="home-logo">logo</div>
+      <h1>The Photobooth</h1>
+
+      <div className="button-container">
+        <button className="button" onClick={navigateToPhotobooth}>
+          <span>Photo Booth</span>
+        </button>
+        
+        <button className="button" onClick={navigateToPhotostrip}>
+          <span>Photo Strip</span>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
