40e27c4b50665d1736fa394a1c2b6db73a699c3c
Ngan Nguyen <nhnguyen@umass.edu>
2025-05-06 15:20:25 -0400

    change to upload photos

--- frontend/src/pages/Photostrip.tsx
+++ frontend/src/pages/Photostrip.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Photostrip.css';
 
 function Photostrip() {
-  return <h1>Photostrip</h1>;
+  console.log("Photostrip component rendered");
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [photos, setPhotos] = useState<(string | null)[]>([null, null, null]); // state variable and function to update state
+  const clickedIndexRef = useRef<number | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files: File[] | null = event.target.files ? Array.from(event.target.files) : null;
+    if (!files || files.length === 0 || clickedIndexRef.current === null) return;
+
+    const file = files[0];
+    const imageURLs: string = URL.createObjectURL(file);
+
+    const updated = [...photos]
+    updated[clickedIndexRef.current] = imageURLs;
+      
+    setPhotos(updated);
+  }
+
+  const handleClick = (index: number) => {
+    clickedIndexRef.current = index;
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="text-holder">Upload Your Photos!</div>
+  
+      <div className="photos-holders">
+        {photos.map((url, index) => (
+          <div key={index} className='photo-box' onClick={() => handleClick(index)}>
+            {url ? <img src={url} alt={`uploaded-${index}`} className="photos" /> : index + 1}
+          </div>
+        ))}
+      </div>
+  
+      <input type="file" ref={fileInputRef} accept="image/*" multiple style={{ display: 'none' }} onChange={handleFileChange} />
+      <button className="upload-button" onClick={() => {'./Result.tsx'}}>Continue</button>
+    </div>
+  );
 }
 
 export default Photostrip;
